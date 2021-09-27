e48a6cab93b56260b227ee2ee9b1ba95f2bfa02f
dterng928 <bhatarasiri_cud@hotmail.com>
2021-09-27 19:33:53 +0700

    finish 03 variable type

--- chapter01-basic-knowledges/1.1-basic-golang/03-variable-types/main.go
+++ chapter01-basic-knowledges/1.1-basic-golang/03-variable-types/main.go
@@ -8,6 +8,11 @@ import (
 func main() {
 	// 1. Literal types
 	// Note: Literal type copy by value
+
+	// var theString2 string
+	// theString2 = "This is string 2"
+	// theString1 := "This is String"
+
 	theString := "This variable type string"
 	theInt := 305
 	theBool := true
@@ -19,6 +24,15 @@ func main() {
 	// printString(theInt) // Compile error
 
 	// 3. interface{} type can accept all types
+	var interf interface{}
+	interf = theString
+	theString2 := interf.(string)
+
+	interf = theInt
+	interf = theBool
+
+	printString(theString2)
+
 	printInterface(theString)
 	printInterface(theInt)
 	printInterface(theBool)
@@ -26,15 +40,28 @@ func main() {
 
 	// 4. map type
 	// Note: Map variable is the pointer of map, pass by pointer of map
-	theMap := map[string]interface{}{}
+
+	// map[Key type]value type{}{} (last branket for initial instance)
+	// theMap := map[string]interface{}{} 
+	theMap := make(map[string]interface{})
 	theMap["firstname"] = "Chaiyapong"
 	theMap["lastname"] = "Lapliengtrakul"
 	theMap["citizen_id"] = "1234"
+
+	theMapInt := map[string]int{}
+	theMapInt["number1"] = 1
+	theMapInt["number2"] = 2
+	printMapInt(theMapInt)
+
 	printMap(theMap)
-	// updateMap(theMap) // This will update map that pass in to function
+	updateMap(theMap) // This will update map that pass in to function (pass by reference)
 	printMapAsJSON(theMap)
+
+	// citizenId := theMap["citizen_id"] // citizenId is interface type; if theMap not have citizen_id(Key) -> see line 64 
+	// citizenIdStr = citizenId.(string) // convert interface to String
+
 	// Note: We can use 2 variables to get map value
-	gender, ok := theMap["gender"]
+	gender, ok := theMap["gender"] // ok is parameter for check key is valid
 	if ok {
 		fmt.Println("gender is ", gender.(string)) // Casting
 	} else {
@@ -46,16 +73,17 @@ func main() {
 	// 5. slice type (dynamic array)
 	// Note: We should name slice phurally
 	// Note: Slices pass by pointer of slice (just like map)
-	theSlices := []string{}
+	// theSlices := []string{}
+	theSlices := make([]string, 0) // another to initial slices (second parameter is slice's size)
 	theSlices = append(theSlices, "item 1")
 	theSlices = append(theSlices, "item 2")
 	theSlices = append(theSlices, "item 3")
 	printSlice(theSlices)
-	// updateSliceIndex0(theSlices) // This will update value of index 0 of slice
+	updateSliceIndex0(theSlices) // This will update value of index 0 of slice ; like map pass by reference
 	printSliceAsJSON(theSlices)
 	printUnderline()
 
-	// 6. Struct type
+	// 6. Struct type for object
 	// Note: It is my practice to always create struct variable as pointer (*Type)
 	//   to avoid confusion in team member, so will can always assume that strut is a pointer
 	theCitizen := &Citizen{
@@ -63,6 +91,11 @@ func main() {
 		Lastname:  "Lapliengtrakul",
 		CitizenID: "1234",
 	}
+	// theCitizen1 := &Citizen{ // has pointer -> pass by reference
+	// 	CitizenID: "567",
+	// } 
+	// theCitizen2 := Citizen{} // no pointer
+
 	printCitizen(theCitizen)
 	// Note: JSON marshal will use struct tag
 	printCitizenAsJSON(theCitizen)
@@ -79,6 +112,10 @@ func main() {
 	theSlices = nil
 	theCitizen = nil
 
+	// var theMap1 map[string]interface{}{} // default nil 
+	// var theSlices1 []string // default nil 
+	// var theCitizen *Citizen // default nil 
+
 	// Note: assign value to nil map and struct will cause runtime error
 	// theMap["key"] = "value" // Runtime Error
 	// theCitizen.Firstname = "Chaiyapong" // Runtime Error
@@ -87,19 +124,25 @@ func main() {
 	theSlices = append(theSlices, "value") // OK
 
 	// 8. Check nil or zero using len()
-	if len(theMap) == 0 {
+	if len(theMap) == 0 { // can check nill or 0 size
 		fmt.Println("theMap is nil")
 	}
+	if theMap == nil { // check nill only
+		fmt.Println("theMap is nil")
+	}
+
 	if len(theSlices) == 0 {
 		fmt.Println("theSlices is nil")
 	}
-	if theCitizen == nil {
+	if theCitizen == nil { // checl nill in struc (only 1 way)
 		fmt.Println("theCitizen is nil")
 	}
 	printUnderline()
 
 	// 9. Enum is just constant in golang
-	theGender := Male
+	var theGender GenderType 
+	// theGender := Male
+	theGender = Male
 	theGender = Female
 	theGender = Unspecify
 	switch theGender {
@@ -134,6 +177,10 @@ func printInterface(input interface{}) {
 	fmt.Println("interface = ", input)
 }
 
+func printMapInt(input map[string]int) {
+	fmt.Println("map = ", input)
+}
+
 func printMap(input map[string]interface{}) {
 	fmt.Println("map = ", input)
 }
@@ -143,8 +190,8 @@ func updateMap(input map[string]interface{}) {
 }
 
 func printMapAsJSON(input map[string]interface{}) {
-	js, _ := json.Marshal(input)
-	fmt.Println("map JSON = ", string(js))
+	js, _ := json.Marshal(input) // any to JSON
+	fmt.Println("map JSON = ", string(js)) // print string
 }
 
 func printSlice(input []string) {
